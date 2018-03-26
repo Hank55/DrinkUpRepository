4bc85aee45d75ecdb3c29c6675b4e4091c7a8be6
Charles-Oscar Combes <charles-oscar.combes@hotmail.com>
2018-03-26 11:50:13 +0200

    Revert "Oscar's name!"
    This reverts commit 005f2eabf099830f8b25f55cd7ef9117fef58115.

--- DrinkUpProject/DrinkUpProject/Startup.cs
+++ DrinkUpProject/DrinkUpProject/Startup.cs
@@ -15,7 +15,6 @@ namespace DrinkUpProject
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            //OSCAR ÄGER!
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
