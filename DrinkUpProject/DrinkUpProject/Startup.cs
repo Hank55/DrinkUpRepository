289d18e053c55d91acc2a88e41199e95e8498116
tyskas <asatysk@gmail.com>
2018-03-26 11:45:25 +0200

    Add a comment

--- DrinkUpProject/DrinkUpProject/Startup.cs
+++ DrinkUpProject/DrinkUpProject/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
+//Åsa säger: Hej!
+
 namespace DrinkUpProject
 {
     public class Startup
