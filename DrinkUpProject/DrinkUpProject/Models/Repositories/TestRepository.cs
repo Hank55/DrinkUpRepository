2305e5d7335b55746a1d5952737fa563af396956
Simon Westberg <simme.westberg@hotmail.com>
2018-03-29 11:30:20 +0200

    added prop in VM and fixed short info for searchresult

--- DrinkUpProject/DrinkUpProject/Models/Repositories/TestRepository.cs
+++ DrinkUpProject/DrinkUpProject/Models/Repositories/TestRepository.cs
@@ -79,32 +79,6 @@ namespace DrinkUpProject.Models.Repositories
              
         }
 
-        internal void AddUser(HomeCreateUserVM model)
-        {
-            User user = new User
-            {
-                FirstName = model.FirstName,
-                LastName = model.LastName,
-                Email = model.Email,
-                Password = model.Password,
-                FavouriteDrink = model.FavouriteDrink
-            };
-            users.Add(user);
-        }
-
-        private string ToShortInfo(string strInstructions)
-        {
-            var splitInfo = strInstructions.Split(" ");
-            var shortInfo = "";
-            for (int i = 0; i < 7; i++)
-            {
-                shortInfo += splitInfo[i] + " ";
-            }
-
-            return shortInfo += "...";
-
-        }
-
         private void SaveToSearchResultList(HomeResultVM[] listResults)
         {
             searchResultListings.Add(listResults);
