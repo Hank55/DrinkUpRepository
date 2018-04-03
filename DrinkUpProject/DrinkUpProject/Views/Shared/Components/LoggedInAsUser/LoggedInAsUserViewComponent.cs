6e2f0148b0319cd39503e3a259ce6cf7bd733bc9
Henrik <henrik_s5@hotmail.comcleargit config --global user.email henrik_s5@hotmail.com>
2018-04-03 18:29:41 +0200

    Logged in as works

---
+++ DrinkUpProject/DrinkUpProject/Views/Shared/Components/LoggedInAsUser/LoggedInAsUserViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using DrinkUpProject.Models.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrinkUpProject.Views.Shared.Components.LoggedInAsUser
+{
+    public class LoggedInAsUserViewComponent : ViewComponent
+    {
+        private AccountRepository repository;
+
+        public LoggedInAsUserViewComponent(AccountRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(repository.GetLoggedInUser(User.Identity));
+        }
+    }
+
+
+}
