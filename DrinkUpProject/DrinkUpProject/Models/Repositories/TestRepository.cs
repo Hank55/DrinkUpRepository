107a4e9290dc0e5d91bd8548157c8bc4ed1c7cef
tyskas <asatysk@gmail.com>
2018-04-03 15:08:48 +0200

    update search result for ingredient search

--- DrinkUpProject/DrinkUpProject/Models/Repositories/TestRepository.cs
+++ DrinkUpProject/DrinkUpProject/Models/Repositories/TestRepository.cs
@@ -67,7 +67,48 @@ namespace DrinkUpProject.Models.Repositories
             {
                 var findDrinkByIdURL = $"https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i={drinkList[i].idDrink}";
                 var tempListDrink = await GetDrinks(findDrinkByIdURL);
-                tempArray.Add(new Drink { strDrink = tempListDrink[0].strDrink, strDrinkThumb = tempListDrink[0].strDrinkThumb, strInstructions = tempListDrink[0].strInstructions });
+                Drink drink = tempListDrink[0];
+                tempArray.Add(new Drink
+                { strDrink = drink.strDrink,
+                    strDrinkThumb = drink.strDrinkThumb,
+                    strInstructions = drink.strInstructions,
+                    strIngredient1 = drink.strIngredient1,
+                    strIngredient2 = drink.strIngredient2,
+                    strIngredient3 = drink.strIngredient3,
+                    strIngredient4 = drink.strIngredient4,
+                    strIngredient5 = drink.strIngredient5,
+                    strIngredient6 = drink.strIngredient6,
+                    strIngredient7 = drink.strIngredient7,
+                    strIngredient8 = drink.strIngredient8,
+                    strIngredient9 = drink.strIngredient9,
+                    strIngredient10 = drink.strIngredient10,
+                    strIngredient11 = drink.strIngredient11,
+                    strIngredient12 = drink.strIngredient12,
+                    strIngredient13 = drink.strIngredient13,
+                    strIngredient14 = drink.strIngredient14,
+                    strIngredient15 = drink.strIngredient15,
+                    strMeasure1 = drink.strMeasure1,
+                    strMeasure2 = drink.strMeasure2,
+                    strMeasure3 = drink.strMeasure3,
+                    strMeasure4 = drink.strMeasure4,
+                    strMeasure5 = drink.strMeasure5,
+                    strMeasure6 = drink.strMeasure6,
+                    strMeasure7 = drink.strMeasure7,
+                    strMeasure8 = drink.strMeasure8,
+                    strMeasure9 = drink.strMeasure9,
+                    strMeasure10 = drink.strMeasure10,
+                    strMeasure11 = drink.strMeasure11,
+                    strMeasure12 = drink.strMeasure12,
+                    strMeasure13 = drink.strMeasure13,
+                    strMeasure14 = drink.strMeasure14,
+                    strMeasure15 = drink.strMeasure15,
+                    strIBA = drink.strIBA,
+                    strAlcoholic = drink.strAlcoholic,
+                    strCategory = drink.strCategory,
+                    strGlass = drink.strGlass,
+                    dateModified = drink.dateModified,
+                    idDrink= drink.idDrink
+                });
             }
 
             return tempArray;
