b30e342a30487f0faca11a24563114f8e469c284
Henrik <henrik_s5@hotmail.comcleargit config --global user.email henrik_s5@hotmail.com>
2018-04-04 14:46:40 +0200

    Trying with navbar-mypage :(

---
+++ DrinkUpProject/DrinkUpProject/Views/Shared/Components/NavBarLoggedIn/NavBarLoggedInViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using DrinkUpProject.Models.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrinkUpProject.Views.Shared.Components.NavBarLoggedIn
+{
+    public class NavBarLoggedInViewComponent
+    {
+        public class LoggedInAsUserViewComponent : ViewComponent
+        {
+            private AccountRepository repository;
+
+            public LoggedInAsUserViewComponent(AccountRepository repository)
+            {
+                this.repository = repository;
+            }
+
+
+            public IViewComponentResult InvokeAsync()
+            {
+                return View(repository.GetLoggedInUser(User.Identity));
+            }
+        }
+    }
+}
